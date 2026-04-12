f71754fe684742c5df5cba0f7c4412330d6b69cf
Sudharsan <tsu3010@gmail.com>
2026-04-12 19:54:42 +0800

    fix_mart_granularity

--- dbt/models/marts/mart_affordability_index.sql
+++ dbt/models/marts/mart_affordability_index.sql
@@ -19,24 +19,11 @@ by_town as (
         flat_type,
         count(*)                                                        as transaction_count,
         round(avg(resale_price), 0)                                     as avg_resale_price,
-        round(percentile_cont(resale_price, 0.5) over (
-            partition by town, flat_type), 0)                           as median_resale_price,
+        round(approx_quantiles(resale_price, 2)[offset(1)], 0)         as median_resale_price,
         round(avg(floor_area_sqm), 1)                                   as avg_floor_area_sqm,
         round(avg(resale_price / nullif(floor_area_sqm, 0)), 0)         as avg_price_per_sqm
     from recent
-    group by town, flat_type, resale_price
-),
-
-deduped as (
-    select distinct
-        town,
-        flat_type,
-        transaction_count,
-        avg_resale_price,
-        median_resale_price,
-        avg_floor_area_sqm,
-        avg_price_per_sqm
-    from by_town
+    group by town, flat_type
 )
 
 select
@@ -47,6 +34,5 @@ select
     median_resale_price,
     avg_floor_area_sqm,
     avg_price_per_sqm,
-    -- Affordability index: lower = more affordable relative to floor area
     round(median_resale_price / nullif(avg_floor_area_sqm, 0), 0)       as affordability_index
-from deduped
+from by_town
--- dbt/models/marts/mart_hdb_by_month.sql
+++ dbt/models/marts/mart_hdb_by_month.sql
@@ -31,25 +31,13 @@ aggregated as (
         flat_type,
         count(*)                                                        as transaction_count,
         round(avg(resale_price), 0)                                     as avg_resale_price,
-        round(percentile_cont(resale_price, 0.5) over (
-            partition by month, town, flat_type), 0)                    as median_resale_price,
+        round(approx_quantiles(resale_price, 2)[offset(1)], 0)         as median_resale_price,
         round(avg(resale_price / nullif(floor_area_sqm, 0)), 0)         as avg_price_per_sqm,
         round(avg(floor_area_sqm), 1)                                   as avg_floor_area_sqm,
         avg(latitude)                                                   as latitude,
         avg(longitude)                                                  as longitude
     from joined
-    group by month, town, flat_type, resale_price
+    group by month, town, flat_type
 )
 
-select distinct
-    month,
-    town,
-    flat_type,
-    transaction_count,
-    avg_resale_price,
-    median_resale_price,
-    avg_price_per_sqm,
-    avg_floor_area_sqm,
-    latitude,
-    longitude
-from aggregated
+select * from aggregated
